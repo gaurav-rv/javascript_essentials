b3156b679e2ca1fb1f5af8d155a8e8ece864e446
gaurav-rv <graghava@uci.edu>
2017-05-12 10:41:25 -0700

    Initial commit

---
+++ style.php
@@ -0,0 +1,58 @@
+<style>
+input[type=submit] {
+  width: 170px;
+  border: 1px dotted #999;
+  border-radius: 0;
+  font-family: Rockwell,Courier Bold,Courier,Georgia,Times,Times New Roman,serif;
+  -webkit-appearance: none;
+}
+body {
+   background: url('http://www.hdbloggers.net/wp-content/uploads/2016/11/Wood-Wallpapers.jpg') center center;
+   font-family: Rockwell,Courier Bold,Courier,Georgia,Times,Times New Roman,serif;
+}
+h5{
+	color: white;
+    	text-align: center;
+	font-family: Rockwell,Courier Bold,Courier,Georgia,Times,Times New Roman,serif;
+}
+div.content{
+        color: white;
+    text-align: center;
+        height:2000px;
+        background:#000000;
+        width:80%;
+        margin:0px auto;
+
+}
+div.container{
+	color: white;
+    text-align: center;
+	height:2000px;
+	background:#000000;
+	width:80%;
+	margin:0px auto;
+}
+
+div.headers{
+	background:#000000;
+	height:50%;
+	width:100%;
+	margin:0px auto;
+}
+
+div.para{
+	background:#000000;
+	height: 100%;
+	width: 50%;
+	margin:0px auto;
+}
+h1 {
+    color: white;
+    text-align: center;
+    font-family: Rockwell,Courier Bold,Courier,Georgia,Times,Times New Roman,serif;
+}
+
+p {
+    font-size: 20px;
+}
+<style>
